cadedd518f4f853cd11bff05ddb15c829819e669
ashish <invoxico.ashish@gmail.com>
2023-07-19 18:49:19 +0530

    Update Db.js

--- config/Db.js
+++ config/Db.js
@@ -4,10 +4,9 @@ const DBconnect = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_URI);
     console.log("database is connected");
-  } catch (err) {}
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-
 module.exports = DBconnect;
-
-
