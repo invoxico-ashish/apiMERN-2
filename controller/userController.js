bfdfee156571a1f500e3350c6ead20da98f80719
ashish <invoxico.ashish@gmail.com>
2023-07-19 12:26:41 +0530

    a

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,21 @@
-exports.updateUserController = ()=>{
-    
-}
+const UserModel = require("../model/UserModel");
+
+exports.updateUserController = async (req, res, next) => {
+  const { name, email, lastname, location } = req.body;
+  console.log(req.body)
+  if (!req.body) {
+    next("Please provide all field");
+  }
+  const user = await UserModel.findOne({ id: req._id });
+  user.name = name;
+  user.lastName = lastname;
+  user.email = email;
+  user.location = location;
+
+  await user.save();
+  const token = user.createJWT();
+  res.status(200).json({
+    user,
+    token,
+  });
+};
--- middleware/ErrMiddleware.js
+++ middleware/ErrMiddleware.js
@@ -1,24 +1,24 @@
 const ErrorMiddleware = (err, req, res, next) => {
   console.log(err);
-  const defaulterr = {
+  const defaultErr = {
     statusCode: 500,
     message: err,
   };
 
   // missing field error
   if (err.name === "validationError") {
-    defaulterr.statusCode = 400;
-    defaulterr.message = Object.values(err.errors)
+    defaultErr.statusCode = 400;
+    defaultErr.message = Object.values(err.errors)
       .map((item) => item.message)
       .join(",");
   }
 
   //duplicate error
   if (err.code && err.code === 11000) {
-    defaulterr.statusCode = 400;
-    defaulterr.message = `${Object.keys(err.keyValue)} Field has to be unique`;
+    defaultErr.statusCode = 400;
+    defaultErr.message = `${Object.keys(err.keyValue)} Field has to be unique`;
   }
 
-  res.status(defaulterr.statusCode).json({ message: defaulterr.message });
+  res.status(defaultErr.statusCode).json({ message: defaultErr.message });
 };
 module.exports = ErrorMiddleware;
